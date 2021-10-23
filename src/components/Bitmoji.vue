bc41c59dccd2455ae9906720b0ecfbaec50eeb47
Will Pak Poy <0521858@radford.act.edu.au>
2021-10-23 14:51:42 +1100

    try this

--- src/components/Bitmoji.vue
+++ src/components/Bitmoji.vue
@@ -1,9 +1,9 @@
 <template>
-  <img class="bitmoji" :src="getBitmoji">
+  <img class="bitmoji" :src="getBitmoji" alt="my bitmoji">
 </template>
 
 <script>
-import libmoji from "libmoji";
+import {getComicId, getAvatarId, randOutfit, buildRenderUrl, randTemplate, templates, getOutfits, randBrand, getBrands} from "libmoji";
   export default {
     name: "bitmoji",
     data: function () {
@@ -13,11 +13,11 @@ import libmoji from "libmoji";
     },
     computed: {
       getBitmoji() {
-        let comicId = libmoji.getComicId(libmoji.randTemplate(libmoji.templates));
-        let avatarId = libmoji.getAvatarId("https://render.bitstrips.com/v2/cpanel/8968038-193774737_66-s5-v1.png?transparent=1&palette=1");
-        let outfit = libmoji.randOutfit(libmoji.getOutfits(libmoji.randBrand(libmoji.getBrands("male"))));
+        let comicId = getComicId(randTemplate(templates));
+        let avatarId = getAvatarId("https://render.bitstrips.com/v2/cpanel/8968038-193774737_66-s5-v1.png?transparent=1&palette=1");
+        let outfit = randOutfit(getOutfits(randBrand(getBrands("male"))));
         
-        return libmoji.buildRenderUrl(comicId,avatarId,1,2,outfit);
+        return buildRenderUrl(comicId,avatarId,1,2,outfit);
       }
     }
   }
