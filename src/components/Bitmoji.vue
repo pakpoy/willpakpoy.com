295792f1326032900ccf4309cba3fb8aa4415530
Will Pak Poy <0521858@radford.act.edu.au>
2021-10-23 12:37:03 +1100

    add the scary bitmoji

---
+++ src/components/Bitmoji.vue
@@ -0,0 +1,24 @@
+<template>
+  <img class="bitmoji" :src="getBitmoji">
+</template>
+
+<script>
+import libmoji from "libmoji";
+  export default {
+    name: "bitmoji",
+    data: function () {
+      return {
+        hello: "Hi!"
+      }
+    },
+    computed: {
+      getBitmoji() {
+        let comicId = libmoji.getComicId(libmoji.randTemplate(libmoji.templates));
+        let avatarId = libmoji.getAvatarId("https://render.bitstrips.com/v2/cpanel/8968038-193774737_66-s5-v1.png?transparent=1&palette=1");
+        let outfit = libmoji.randOutfit(libmoji.getOutfits(libmoji.randBrand(libmoji.getBrands("male"))));
+        
+        return libmoji.buildRenderUrl(comicId,avatarId,1,2,outfit);
+      }
+    }
+  }
+</script>
